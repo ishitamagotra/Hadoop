3287074db0f5c5b6edefbba77b9d669013783973
Ishita Magotra <imagotra@hawk.iit.edu>
2017-09-28 10:06:54 -0500

    Update MaxVisibilityMapper.java

--- ITMD521/Week-05/item-one/MaxVisibilityMapper.java
+++ ITMD521/Week-05/item-one/MaxVisibilityMapper.java
@@ -10,7 +10,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class MaxVisibilityMapper
   extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-  private static final int MISSING = 9999;
+  private static final int MISSING = 99999;
   
   @Override
   public void map(LongWritable key, Text value, Context context)
@@ -18,15 +18,13 @@ public class MaxVisibilityMapper
     
     String line = value.toString();
     String year = line.substring(15, 19);
-    int airVisibility;
-    if (line.charAt(87) == '+') { // parseInt doesn't like leading plus signs
-      airVisibility = Integer.parseInt(line.substring(88, 92));
-    } else {
-      airVisibility = Integer.parseInt(line.substring(87, 92));
-    }
-    String quality = line.substring(92, 93);
-    if (airVisibility != MISSING && quality.matches("[01459]")) {
-      context.write(new Text(year), new IntWritable(airVisibility));
+    int vd;
+    vd = Integer.parseInt(line.substring(78, 83));
+    String vid = line.substring(83, 84);
+    
+    if (vd != MISSING && vid.matches("[01459]")){
+        context.write(new Text(year), new IntWritable(vd));
+    
     }
   }
 }
