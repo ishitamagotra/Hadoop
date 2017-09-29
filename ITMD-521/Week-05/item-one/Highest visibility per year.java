88071ac3dfe8740d26837ef38eb76872ef75251f
Ishita Magotra <imagotra@hawk.iit.edu>
2017-09-29 15:18:27 -0500

    Update and rename MaxVisibility.java to Highest visibility per year.java

--- ITMD-521/Week-05/item-one/Highest visibility per year.java
+++ ITMD-521/Week-05/item-one/Highest visibility per year.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class MaxVisibility {
+public class Highest visibility per year {
 
   public static void main(String[] args) throws Exception {
     if (args.length != 2) {
